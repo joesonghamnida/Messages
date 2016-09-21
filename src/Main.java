7c6ae33a852a51602fde9be614fb376ee9ecf6e3
joesonghamnida <joemorris2430@gmail.com>
2016-09-21 09:09:12 -0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Spark.staticFileLocation("/public");
         Spark.init();
 
         //three parameters: path, route, response transformer
